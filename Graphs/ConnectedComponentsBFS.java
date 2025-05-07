6078c96a1a263e01df53207b9528e02fddd562b7
Srinivas <lingampellisrinivas3036@gmail.com>
2025-05-07 15:26:27 +0530

    updated code

--- Graphs/ConnectedComponentsBFS.java
+++ Graphs/ConnectedComponentsBFS.java
@@ -4,6 +4,56 @@ class ConnectedComponentsBFS
 {
     public static ArrayList<ArrayList<Integer>> adList;
 
+
+
+        public static void buildAdjList(int n,int [][]edges)
+        {
+            adList=new ArrayList<>();
+
+            for(int i=0;i<n;i++)
+            adList.add(new ArrayList<>());
+
+            for(int []edge: edges)
+            {
+                int u=edge[0];
+                int v=edge[1];
+
+                adList.get(u).add(v);
+                adList.get(v).add(u);
+
+            }
+        }
+
+
+        public static void bfs(int start,boolean[]visited)
+        {
+            Queue<Integer>q=new LinkedList<>();
+
+            q.offer(start);
+
+            visited[start]=true;
+
+
+            while(!q.isEmpty())
+            {
+                int node=q.poll();
+            
+
+
+                for(int nbr:adList.get(node))
+                {
+                    if(!visited[nbr])
+                    {
+                        q.offer(nbr);
+                        visited[nbr]=true;
+                    }
+                }
+            }
+
+        }
+
+
+
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
@@ -17,7 +67,7 @@ class ConnectedComponentsBFS
             for(int j=0;j<2;j++)
             {
                 edges[i][j]=sc.nextInt();
-                
+
             }
         }
     }
